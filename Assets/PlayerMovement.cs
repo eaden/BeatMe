ac8ed5630e7364ee65c3d12028838d491b38dab1
K K <kmk@gmx.de>
2022-06-24 20:33:17 +0200

    Movement erster Schritt

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Rigidbody2D rig;
+    float speed = 10;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 movement = new Vector2(0,0);
         if(Input.GetKey(KeyCode.UpArrow))
         {
-            gameObject.transform.position += new Vector3(1, 0,0);
+            movement.y = 1;
         }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            movement.y = -1;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            movement.x = -1;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            movement.x = 1;
+        }
+
+        rig.velocity += movement*speed*Time.deltaTime;
     }
 }
